0fa261a80d7d039f753d7c3cf907d4e8a9c60f1c
John <johnrubio93@gmail.com>
2019-06-19 23:32:37 +1000

    installed session related packages and created user model and schema

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const expressSession = require("express-session");
+const MongoStore = require("connect-mongo")(expressSession);
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const passport = require("passport");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/pigeondex", { useNewUrlParser: true });
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 mongoose.connection.on("error", err => console.log(err));
@@ -18,9 +22,23 @@ app.set("view engine", "handlebars");
 
 app.use(methodOverride('_method', { methods: ['POST', 'GET']}));
 
+app.use(expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+require("./config/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(require("./routes"));
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
