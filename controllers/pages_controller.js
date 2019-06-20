f6ac29ce834454d18868308f667738133ba65e0b
John <johnrubio93@gmail.com>
2019-06-20 15:18:19 +1000

    updated navbar and dashboard to use session information for user

--- controllers/pages_controller.js
+++ controllers/pages_controller.js
@@ -6,8 +6,13 @@ function about(req, res) {
   res.render("pages/about");
 }
 
-function dashboard(req, res) {
-  res.render("pages/dashboard");
+async function dashboard(req, res) {
+  // const {username, email, password, rank} = req.body;
+  // const user = await UserModel.create({username, email, password, rank});
+  const user = req.session.user;
+  console.log("testing dashboard",user);
+  // req.session.user = user;
+  res.render("pages/dashboard", {user});
 }
 
 module.exports = {
