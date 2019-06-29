73e2d2065dbc767b50d708332a6077cd45c38d8c
John <johnrubio93@gmail.com>
2019-06-29 23:03:27 +1000

    added routes for login and register in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="navWrapper">
             <nav>
                 <p class="navRank"><span className="bold">Encounters: 10</span></p>
-                <p class="navLinks"><a href="/pigeons">Index</a> | <a href="/login">Login</a> | <a href="/logout">Logout</a></p>
-                <p class="navUser"><span className="bold">Rank:</span> Pigeon Watcher | <a href="/dashboard"><span className="bold">John Rubio</span></a></p>
+                <p class="navLinks"><Link to="/pigeons">Index</Link> | <Link to="/login">Login</Link> | <Link to="/logout">Logout</Link></p>
+                <p class="navUser"><span className="bold">Rank:</span> Pigeon Watcher | <Link to="/dashboard"><span className="bold">John Rubio</span></Link></p>
             </nav>
         </div>
     );
-}
+};
 
 
 export default Navbar;
